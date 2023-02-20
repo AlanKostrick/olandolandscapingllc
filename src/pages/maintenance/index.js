6b2f363df047270c7f8bbd9d8b0d4d348ec1dd96
AlanKostrick <alankostrick@gmail.com>
2023-02-19 21:07:33 -0500

    add all placeholder pages and routes

---
+++ src/pages/maintenance/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './style.module.css';
+
+const Maintenance = () => {
+    return (
+        <div className={styles.container}>Maintenance</div>
+    )
+}
+
+export default Maintenance;
